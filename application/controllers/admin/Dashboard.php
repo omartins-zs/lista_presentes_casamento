9c57413857fa15f2fbb23751879ae430db337f76
omartins-zs <gabrielmatheus209@gmail.com>
2024-08-29 17:27:14 -0300

    Criando Controller Dashboard

---
+++ application/controllers/admin/Dashboard.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends MY_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$dados['total_produtos'] = $this->Dashboard_model->countProdutos();
+		$dados['total_marcas'] = $this->Dashboard_model->countMarcas();
+
+		$this->load->vars($dados);
+
+		$this->load->view('templates/admin/header');
+		$this->load->view('templates/admin/navbar');
+		$this->load->view('admin/dashboard');
+		$this->load->view('templates/admin/footer');
+	}
+}
