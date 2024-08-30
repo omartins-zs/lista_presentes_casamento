809c9180b92dd7946aea981e6a1ccbbb5b14b150
omartins-zs <gabrielmatheus209@gmail.com>
2024-08-30 15:54:13 -0300

    Criado o Navbar do Shop

---
+++ application/views/templates/shop/navbar.php
@@ -0,0 +1,14 @@
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	<div class="container-fluid px-4 px-lg-5">
+		<a class="navbar-brand" href="#!">Gabriel & Barbara</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+				<li class="nav-item"><a class="nav-link active" href="#!" aria-current="page">Lista de Presentes</a></li>
+				<li class="nav-item"><a class="nav-link" href="#!">Cerimônia</a></li>
+				<li class="nav-item"><a class="nav-link" href="#!">Festa</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
