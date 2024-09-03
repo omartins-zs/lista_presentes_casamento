984b3c026c03871c10c0116a1ac361ef02eca9b4
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-03 17:09:52 -0300

    Criando Template Header do Shop

---
+++ application/views/templates/shop/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<meta name="description" content="" />
+	<meta name="author" content="Gabriel Martins" />
+	<title><?= $titulo ?></title>
+	<!-- Favicon-->
+	<link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" crossorigin="anonymous" />
+
+	<script src="https://kit.fontawesome.com/cba92a6c1e.js" crossorigin="anonymous"></script>
+
+
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css" />
+	<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+
+
+	<!-- Core theme CSS (includes Bootstrap)-->
+	<link href="<?= base_url() ?>assets/css/styles.css" rel="stylesheet" />
+
+	<!-- SHOP Personalizado -->
+	<link href="<?= base_url() ?>assets/shop/css/style.css" rel="stylesheet" />
+</head>
+
+<body>
