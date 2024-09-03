141cb98a8fce9af34cc5fd3046375e72c43907cf
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-03 17:09:34 -0300

    Criando Template Footer do Shop

---
+++ application/views/templates/shop/footer.php
@@ -0,0 +1,16 @@
+ <!-- Footer-->
+ <footer class="py-3 bg-dark">
+ 	<div class="container">
+	 <p class="text-light text-center m-0">
+	 Copyright &copy;
+ 			<?= date('Y') ?> Todos os direitos reservados | <i class="icon-heart" aria-hidden="true"></i> by <a href="https://gabrielmartinsdev.com.br/" target="_blank" class="text-primary">Gabriel Martins Desenvolvimento</a>
+ 		</p>
+ 	</div>
+ </footer>
+ <!-- Bootstrap core JS-->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+ <!-- Core theme JS-->
+ <!-- <script src="js/scripts.js"></script> -->
+ </body>
+
+ </html>
