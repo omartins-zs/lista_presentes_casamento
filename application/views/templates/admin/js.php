59b794c8ed7976efccd98aba274f24e352c176ed
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-03 17:37:55 -0300

    Inicializando e Configurando o Datatable

--- application/views/templates/admin/js.php
+++ application/views/templates/admin/js.php
@@ -17,9 +17,42 @@
     	$(document).ready(function() {
     		// Inicializar Bootstrap Select
     		$('.selectpicker').selectpicker();
+
+    		$('#dataTable').DataTable({
+    			"ordering": true,
+    			// "paging": true,
+    			"paging": false,
+    			"scrollCollapse": true,
+    			"scrollY": '50vh',
+    			"searching": true,
+    			"oLanguage": {
+    				"sEmptyTable": "Nenhum registro encontrado na tabela",
+    				"sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+    				"sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+    				"sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+    				"sInfoPostFix": "",
+    				"sInfoThousands": ".",
+    				"sLengthMenu": "Mostrar _MENU_ registros por pagina",
+    				"sLoadingRecords": "Carregando...",
+    				"sProcessing": "Processando...",
+    				"sZeroRecords": "Nenhum registro encontrado",
+    				"sSearch": "",
+    				"oPaginate": {
+    					"sNext": "Proximo",
+    					"sPrevious": "Anterior",
+    					"sFirst": "Primeiro",
+    					"sLast": "Ultimo"
+    				},
+
+    				"oAria": {
+    					"sSortAscending": ": Ordenar colunas de forma ascendente",
+    					"sSortDescending": ": Ordenar colunas de forma descendente"
+    				}
+    			}
+    		});
     	});
     </script>
-	
+
     <script type="text/javascript">
     	$(document).ready(function() {
     		<?php if ($this->session->flashdata("success")) { ?>
