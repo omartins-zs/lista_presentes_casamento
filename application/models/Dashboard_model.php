61493fab1febe51fcfcb90b20edf902dbca2c5b5
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-03 17:41:56 -0300

    Criando o Metodo de Count das Marcas

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -12,4 +12,14 @@ class Dashboard_model extends CI_Model
 
 		return $result;
 	}
+
+	public function countMarcas()
+	{
+		$this->db->select('COUNT(*) as TOTAL_MARCAS');
+		$this->db->from('marcas');
+
+		$result = $this->db->get()->row()->TOTAL_MARCAS;
+
+		return $result;
+	}
 }
