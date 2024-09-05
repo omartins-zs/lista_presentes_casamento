9c5d1e0ce43bc4dbdd5ef3704ed772831c9fa545
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-05 00:29:15 -0300

    Ajustando os Links Principais para o Shop

--- application/views/templates/shop/navbar.php
+++ application/views/templates/shop/navbar.php
@@ -1,11 +1,11 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 	<div class="container-fluid px-4 px-lg-5">
-		<a class="navbar-brand" href="#!">Gabriel & Barbara</a>
+		<a class="navbar-brand" href="<?= base_url() ?>shop/">Gabriel & Barbara</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-				<li class="nav-item"><a class="nav-link active" href="#!" aria-current="page">Lista de Presentes</a></li>
+				<li class="nav-item"><a class="nav-link active" href="<?= base_url() ?>shop/" aria-current="page">Lista de Presentes</a></li>
 				<li class="nav-item"><a class="nav-link" href="<?= base_url() ?>shop/cerimonia">Cerimônia</a></li>
 				<li class="nav-item"><a class="nav-link" href="<?= base_url() ?>shop/festa">Festa</a></li>
 			</ul>
