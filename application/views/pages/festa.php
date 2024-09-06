dcfb82d1a437241a7cdbafc27609b5bb7b8d285d
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-05 23:44:49 -0300

    Criando a View da Festa com Img e Mapa Iframe

---
+++ application/views/pages/festa.php
@@ -0,0 +1,30 @@
+<div class="container-fluid my-4">
+	<div class="row">
+		<!-- Imagem do Local -->
+		<div class="col-md-6 d-flex flex-column align-items-center">
+			<h4 class="mt-3 text-center mb-0">Império Eventos</h4>
+
+			<img src="<?= base_url() ?>assets/img/salaoImperio2.jpg" alt="Local" class="h-75 w-100">
+		</div>
+		<!-- Mapa e Informações -->
+		<div class="col-md-6 d-flex flex-column align-items-center mt-5">
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d117128.74815515937!2d-46.56034229118223!3d-23.45060141699759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d-23.429119999999998!2d-46.543667199999994!4m5!1s0x94ce6270c4cec8c1%3A0x9cffec5080df21bc!2sImp%C3%A9rio%20Eventos%20-%20R.%20Ilha%20Bela%2C%20344%20-%20Parque%20das%20Nacoes%2C%20Guarulhos%20-%20SP%2C%2007243-470!3m2!1d-23.4386311!2d-46.4120712!5e0!3m2!1spt-BR!2sbr!4v1725557722049!5m2!1spt-BR!2sbr" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+				<!-- <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3660.879490935019!2d-46.432563!3d-23.42872!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce89f05f3d2a65%3A0xbcc4108183d1b196!2sPar%C3%B3quia%20Santa%20Cruz%20e%20Nossa%20Senhora%20Aparecida!5e0!3m2!1spt-BR!2sus!4v1725077230763!5m2!1spt-BR!2sus" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+			</div>
+			<h3 class="mt-3 text-center">18:00 ás 23:00</h3>
+			<h4 class="mt-3 text-center">R. Ilha Bela, 344 - Parque das Nacoes, Guarulhos - SP, 07243-470</h4>
+			<div class="mt-3 d-flex justify-content-center w-100">
+				<a href="https://maps.app.goo.gl/YJZPtFErMy3BKcdF9" class="btn btn-success mr-2" target="_blank">
+					<i class="bi bi-geo-alt"></i> Google Maps
+				</a>
+				<a href="https://www.waze.com/live-map/directions/br/sp/r.-ilha-bela,-344?to=place.ChIJ5dB6xHBizpQRN5Fvsh_mL7c&from=ll.-23.4675%2C-46.5277&utm_medium=lm_share_directions&utm_campaign=default&utm_source=waze_website" class="btn" target="_blank" style="background-color: #39ceff;">
+					<i class="bi bi-car-front"></i>
+					Waze
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
