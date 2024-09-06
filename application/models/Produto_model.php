cb298fc5800bad510ab9c119f3f9699c3f23fddb
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-05 23:21:42 -0300

    Criando Metodo para Buscar os Produtos

---
+++ application/models/Produto_model.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Produto_model extends CI_Model
+{
+	public function buscaProdutos()
+	{
+		$this->db->select('*');
+		$this->db->from('produtos');
+		$result['produtos'] = $this->db->get()->result();
+
+		$this->db->select('COUNT(*) as TOTAL_PRODUTOS');
+		$this->db->from('produtos');
+		$result['total_produtos'] = $this->db->get()->row()->TOTAL_PRODUTOS;
+
+		return $result;
+	}
+}
