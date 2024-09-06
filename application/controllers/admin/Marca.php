c42972d7b5be673984e01bbecf3e1360edf6be08
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-05 22:55:06 -0300

    Criando Controller de Marca

---
+++ application/controllers/admin/Marca.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Marca extends MY_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$dados["titulo"] = "Marcas";
+		$dados['marcas'] = $this->Marca_model->buscaMarcas();
+
+		$this->load->vars($dados);
+
+		$this->load->view('templates/admin/header');
+		$this->load->view('templates/admin/navbar');
+		$this->load->view('admin/marca/listagem');
+		$this->load->view('templates/admin/footer');
+	}
+}
