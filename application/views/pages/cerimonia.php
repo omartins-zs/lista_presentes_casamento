ad985c9e755b8a0aab39213bb96015a94bc4f09a
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-05 23:44:37 -0300

    Criando a View da Cerimonia com Img e Mapa Iframe

---
+++ application/views/pages/cerimonia.php
@@ -0,0 +1,26 @@
+<div class="container-fluid my-4">
+	<div class="row">
+		<!-- Imagem do Local -->
+		<div class="col-md-6 d-flex flex-column align-items-center">
+			<h4 class="mt-3 text-center mb-0">Paróquia Santa Cruz e Nossa Senhora Aparecida</h4>
+
+			<img src="<?= base_url() ?>assets/img/igreja.jpg" alt="Local" class="h-75 w-100">
+		</div>
+		<!-- Mapa e Informações -->
+		<div class="col-md-6 d-flex flex-column align-items-center mt-5">
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3660.879490935019!2d-46.432563!3d-23.42872!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce89f05f3d2a65%3A0xbcc4108183d1b196!2sPar%C3%B3quia%20Santa%20Cruz%20e%20Nossa%20Senhora%20Aparecida!5e0!3m2!1spt-BR!2sus!4v1725077230763!5m2!1spt-BR!2sus" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+			<h4 class="mt-3 text-center">R. Muritiba, 506 - Jardim Pres. Dutra, Guarulhos - SP, 07173-141, Brasil</h4>
+			<div class="mt-3 d-flex justify-content-center w-100">
+				<a href="https://maps.app.goo.gl/BZSXtjgYxtRkxTpR7" class="btn btn-success mr-2" target="_blank">
+					<i class="bi bi-geo-alt"></i> Google Maps
+				</a>
+				<a href="https://www.waze.com/live-map/directions/br/sp/paroquia-santa-cruz-e-nossa-senhora-aparecida?to=place.ChIJZSo9X_CJzpQRlrHRg4EQxLw&from=ll.-23.4675%2C-46.5277&utm_medium=lm_share_directions&utm_campaign=default&utm_source=waze_website" class="btn" target="_blank" style="background-color: #39ceff;">
+					<i class="bi bi-car-front"></i>
+					Waze
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
