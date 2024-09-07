5ffebf31c1bf578551d446a04d9b2339ca38fc49
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-07 01:12:16 -0300

    Add Configuração do Upload e Log

--- application/controllers/admin/Produto.php
+++ application/controllers/admin/Produto.php
@@ -9,4 +9,17 @@ class Produto extends MY_Controller
 		log_message('debug', 'Construtor da classe Produto chamado');
 		$this->load->library('upload');
 	}
+
+	public function configurarUpload()
+	{
+		log_message('debug', 'Configurando upload');
+
+		$config['upload_path']   = 'assets/admin/upload/';
+		$config['allowed_types'] = 'jpg|png|jpeg';
+		$config['overwrite']     = TRUE;
+		$config['max_size']      = 3072;
+		$config['detect_mime']   = TRUE;
+
+		return $config;
+	}
 }
