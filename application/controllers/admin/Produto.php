37d662d7768c96afadc51e42a319dfb4c4dcdd1b
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-07 01:09:23 -0300

    Inicializando Library Upload e Add Log dos Erros

--- application/controllers/admin/Produto.php
+++ application/controllers/admin/Produto.php
@@ -6,5 +6,7 @@ class Produto extends MY_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		log_message('debug', 'Construtor da classe Produto chamado');
+		$this->load->library('upload');
 	}
 }
