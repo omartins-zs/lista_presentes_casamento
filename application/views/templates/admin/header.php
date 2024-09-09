8de7926218c73a8efa3a27410720d55c906f6cdb
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-09 00:20:16 -0300

    Ajustando os CDN de Links do Header

--- application/views/templates/admin/header.php
+++ application/views/templates/admin/header.php
@@ -2,17 +2,18 @@
 <html lang="en">
 
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<meta name="description" content="" />
 	<meta name="author" content="Gabriel Martins" />
 	<title><?= $titulo ?></title>
+
 	<!-- Favicon -->
 	<link rel="icon" type="image/png" href="<?= base_url() ?>assets/img/icons8-caixa-de-presente-color-favicons/web/icons8-caixa-de-presente-color-16.png" />
 
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous" />
+	<link href="<?= base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet" />
 
 	<!-- Bootstrap Select CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.1/dist/css/bootstrap-select.min.css" rel="stylesheet" />
@@ -27,9 +28,7 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.13.1/css/alertify.min.css" integrity="sha512-IXuoq1aFd2wXs4NqGskwX2Vb+I8UJ+tGJEu/Dc0zwLNKeQ7CW3Sr6v0yU3z5OQWe3eScVIkER4J9L7byrgR/fA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.13.1/css/themes/bootstrap.min.css" integrity="sha512-6xVTeh6P+fsqDhF7t9sE9F6cljMrK+7eR7Qd+Py7PX5QEVVDLt/yZUgLO22CXUdd4dM+/S6fP0gJdX2aSzpkmg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-	<!-- Core theme CSS (includes Bootstrap)-->
-	<link href="<?= base_url() ?>assets/css/styles.css" rel="stylesheet" />
-
+	<!-- Estilos personalizados -->
 	<link href="<?= base_url() ?>assets/admin/css/style.css" rel="stylesheet" />
 
 </head>
--- application/views/templates/shop/header.php
+++ application/views/templates/shop/header.php
@@ -2,17 +2,18 @@
 <html lang="en">
 
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<meta name="description" content="" />
 	<meta name="author" content="Gabriel Martins" />
 	<title><?= $titulo ?></title>
-	<!-- Favicon-->
+
+	<!-- Favicon -->
 	<link rel="icon" type="image/png" href="<?= base_url() ?>assets/img/icons8-caixa-de-presente-color-favicons/web/icons8-caixa-de-presente-color-16.png" />
 
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" crossorigin="anonymous" />
+	<link href="<?= base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet" />
 
 	<!-- Fontawesome -->
 	<script src="https://kit.fontawesome.com/f5b814ac36.js" crossorigin="anonymous"></script>
@@ -24,11 +25,7 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css" />
 
-
-	<!-- Core theme CSS (includes Bootstrap)-->
-	<link href="<?= base_url() ?>assets/css/styles.css" rel="stylesheet" />
-
-	<!-- SHOP Personalizado -->
+	<!-- Estilos personalizados -->
 	<link href="<?= base_url() ?>assets/shop/css/style.css" rel="stylesheet" />
 </head>
 
