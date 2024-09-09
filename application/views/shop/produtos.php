250c92301194ffdff9a771d78ea02da554a5608b
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-08 23:52:57 -0300

    Adicionando o header com Imagem

--- application/views/shop/produtos.php
+++ application/views/shop/produtos.php
@@ -1,15 +1,10 @@
 <!-- Header-->
-<header class="bg-dark py-5">
-	<div class="container px-4 px-lg-5 my-5">
-		<div class="text-center text-white">
-			<h1 class="display-4 fw-bolder">Shop in style</h1>
-			<p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
-		</div>
-	</div>
+<header class="header-image py-5">
+	
 </header>
 
 <!-- Section-->
-<section class="py-5">
+<section class="py-2">
 	<div class="container-fluid px-4 px-lg-5 mt-5">
 		<div class="row gx-4 gx-lg-5 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
 			<?php foreach ($produtos['produtos'] as  $produto  =>     $item) : ?>
