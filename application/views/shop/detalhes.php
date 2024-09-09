c4d6c42bf8b6fefb24dd637a7e0149acc166fd1a
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-08 23:49:21 -0300

    Add Margin nos Cards com Infos de PIX

--- application/views/shop/detalhes.php
+++ application/views/shop/detalhes.php
@@ -100,8 +100,8 @@
 				<p class="mb-4 text-center">Utilize o QR Code acima para fazer a contribuição via PIX.</p>
 
 				<!-- Cartões com Informações de PIX -->
-				<div class="d-flex justify-content-center flex-wrap gap-3">
-					<div class="card mb-4" style="background-color:#f48c06; box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;">
+				<div class="d-flex justify-content-center flex-wrap">
+					<div class="card mb-4 mr-3" style="background-color:#f48c06; box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;">
 						<div class="card-body">
 							<h5 class="card-title">PIX do GABRIEL</h5>
 							<span><strong>Banco:</strong> Nubank</span><br>
