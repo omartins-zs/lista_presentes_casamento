2e798e1a1bda21d06e5b4afba4810f5d55fdda48
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-30 14:42:51 -0300

    Add Script SQL(Dump) para Popular Banco de Dados

--- lista_presentes_casamento_db.sql
+++ lista_presentes_casamento_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 29-Ago-2024 às 20:23
+-- Generation Time: 03-Set-2024 às 20:17
 -- Versão do servidor: 10.1.30-MariaDB
 -- PHP Version: 5.6.33
 
@@ -64,27 +64,30 @@ CREATE TABLE `produtos` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `link_1` varchar(255) DEFAULT NULL,
-  `link_2` varchar(255) DEFAULT NULL
+  `link_2` varchar(255) DEFAULT NULL,
+  `nome_comprador` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `produtos`
 --
 
-INSERT INTO `produtos` (`id`, `nome`, `detalhes`, `imagem`, `preco_intervalo`, `comprado`, `created_at`, `updated_at`, `link_1`, `link_2`) VALUES
-(1, 'Ventilador de mesa', '3 Velocidades \r\n110V', '', 'Até R$ 120,00', 0, '2024-08-29 11:40:57', '2024-08-29 11:40:57', NULL, NULL),
-(2, 'Sanduicheira Grill', '', '', 'Até 100,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(3, 'Microondas', '', '', 'R$ 400,00 - R$ 500,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(4, 'Liquidificador 1000W', '', '', 'Até 130,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(5, 'Chaleira Elétrica', '', '', 'Até 100,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(6, 'Aspirador de Pó', '', '', 'Até R$ 120,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(7, 'Afiador de Facas', '', '', 'Até R$ 50,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(8, 'Kit Churrasco com Tábua', '', '', 'Até R$ 100,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(9, 'Kit de Potes Herméticos', '', '', 'R$ 80,00 - R$ 130,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(10, 'Kit de Utensílios de Cozinha em Silicone', '', '', 'Até R$ 70,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(11, 'Fritadeira Airfryer', '', '', 'R$ 250,00 - R$ 350,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(12, 'Faqueiro Tramontina 24 Peças', '', '', 'Até R$ 100,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL),
-(13, 'Jogo de Facas Tramontina', '', '', 'Até R$ 100,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL);
+INSERT INTO `produtos` (`id`, `nome`, `detalhes`, `imagem`, `preco_intervalo`, `comprado`, `created_at`, `updated_at`, `link_1`, `link_2`, `nome_comprador`) VALUES
+(1, 'Ventilador de mesa', '3 Velocidades 110V', '', 'Até R$ 120,00', 1, '2024-08-29 11:40:57', '2024-09-02 09:17:58', '', '', 'Matheus Alves'),
+(2, 'Sanduicheira Grill', '', '', 'Até 100,00', 1, '2024-08-29 14:12:24', '2024-09-03 16:23:08', NULL, NULL, 'RAQUEL'),
+(3, 'Microondas', '', '', 'R$ 400,00 - R$ 500,00', 1, '2024-08-29 14:12:24', '2024-09-03 16:32:21', NULL, NULL, 'Lucas'),
+(4, 'Liquidificador 1000W', '', '', 'Até 130,00', 1, '2024-08-29 14:12:24', '2024-09-03 16:33:03', NULL, NULL, 'Gabriel'),
+(5, 'Chaleira Elétrica', '', '', 'Até 100,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL, NULL),
+(6, 'Aspirador de Pó', '', '', 'Até R$ 120,00', 1, '2024-08-29 14:12:24', '2024-09-03 17:00:58', NULL, NULL, 'Luander'),
+(7, 'Afiador de Facas', '', '', 'Até R$ 50,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL, NULL),
+(8, 'Kit Churrasco com Tábua', '', '', 'Até R$ 100,00', 1, '2024-08-29 14:12:24', '2024-09-03 17:01:29', NULL, NULL, 'Livia'),
+(9, 'Kit de Potes Herméticos', '', '', 'R$ 80,00 - R$ 130,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL, NULL),
+(10, 'Kit de Utensílios de Cozinha em Silicone', '', '', 'Até R$ 70,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL, NULL),
+(11, 'Fritadeira Airfryer', '', '', 'R$ 250,00 - R$ 350,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL, NULL),
+(12, 'Faqueiro Tramontina 24 Peças', '', '', 'Até R$ 100,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL, NULL),
+(13, 'Jogo de Facas Tramontina', '', '', 'Até R$ 100,00', 0, '2024-08-29 14:12:24', '2024-08-29 14:12:24', NULL, NULL, NULL),
+(17, 'Shorts Feminino Esporte Runner Preta', 'Short', 0x73686f72742e706e67, 'Até R$ 150,00', 0, '2024-08-30 14:46:48', '2024-08-30 14:46:48', 'https://www.caedu.com.br/shorts-fem-esp-feh301a-runner-ptopreto451801361144/p?idsku=164827&gad_source=1&gclid=Cj0KCQjw28W2BhC7ARIsAPerrcIcwxQfoL_nZ_Wf6YIwBPM4AzLRiTMG6yDd3Izh3efYN0_wFsoT80saAiSYEALw_wcB', '', NULL),
+(18, 'Shorts Feminino Esporte Runner Preta', 'Short', 0x73686f72742e706e67, 'Até R$ 150,00', 0, '2024-08-30 14:48:00', '2024-08-30 14:48:00', 'https://www.caedu.com.br/shorts-fem-esp-feh301a-runner-ptopreto451801361144/p?idsku=164827&gad_source=1&gclid=Cj0KCQjw28W2BhC7ARIsAPerrcIcwxQfoL_nZ_Wf6YIwBPM4AzLRiTMG6yDd3Izh3efYN0_wFsoT80saAiSYEALw_wcB', '', NULL);
 
 -- --------------------------------------------------------
 
@@ -96,6 +99,18 @@ CREATE TABLE `produto_marcas` (
   `produto_id` int(11) NOT NULL,
   `marca_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `produto_marcas`
+--
+
+INSERT INTO `produto_marcas` (`produto_id`, `marca_id`) VALUES
+(1, 4),
+(1, 7),
+(18, 1),
+(18, 2),
+(18, 5),
+(18, 6);
 
 --
 -- Indexes for dumped tables
@@ -134,7 +149,7 @@ ALTER TABLE `marcas`
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
